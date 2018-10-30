76c2bfb81bee9ce409e7065645cfed8845cfb18e
363300495 <363300495@qq.com>
2018-10-30 18:56:00 +0800

    11

--- WMall/WMall/Classes/Entrance/AppDelegate.m
+++ WMall/WMall/Classes/Entrance/AppDelegate.m
@@ -18,7 +18,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 	
 	self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-	self.window.backgroundColor = [UIColor whiteColor];
 	self.window.rootViewController = [[MainViewController alloc] init];
 	[self.window makeKeyAndVisible];
 	
@@ -35,7 +34,7 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
 	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
 	// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
+} 
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
